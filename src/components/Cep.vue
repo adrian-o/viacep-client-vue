2a0ba26e9ba7d1e1b57e04afb474b1beb85ed31d
Adriano Ohana <adriano.ohana@gmail.com>
2020-07-06 16:44:45 -0300

    Primeira versão da nossa aplicação cliente

---
+++ src/components/Cep.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" v-model="nome"/>
+    <label for="cep">CEP</label>
+    <input type="text" name="cep" v-model="cep"/>
+    <!--button v-on:click="envia" /-->
+    <button @click="enviaCep">Enviar</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Cep',
+  data() {
+    return {
+      cep: '', 
+      nome: ''
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    async enviaCep() {
+      await axios.post('http://localhost:8080/livrese/cep/endereco', {nome:this.nome, cep:this.cep})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cep msg="Welcome to mycep"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Cep from '@/components/Cep.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Cep
   }
 }
 </script>
