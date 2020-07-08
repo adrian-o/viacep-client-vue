950fea78fd3dfc7bf4cc60acb673ae0e408f7b95
Adriano Ohana <adriano.ohana@gmail.com>
2020-07-07 21:15:49 -0300

    Inclusão Materialize e criação de histórico de consulta

--- src/components/Cep.vue
+++ src/components/Cep.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="hello">
-    <label for="nome">Nome</label>
-    <input type="text" name="nome" v-model="nome"/>
-    <label for="cep">CEP</label>
-    <input type="text" name="cep" v-model="cep"/>
-    <!--button v-on:click="envia" /-->
-    <button @click="enviaCep">Enviar</button>
+    <div class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <input v-model="nome" placeholder="Solicitante" id="nome" type="text" class="validate">
+          <label for="nome">Nome</label>
+        </div>
+        <div class="input-field col s12">
+          <input v-model="cep" placeholder="CEP" id="cep" type="text" class="validate">
+          <label for="cep">CEP</label>
+        </div>
+        <!--button v-on:click="envia" /-->
+        <button @click="enviaCep" class="btn waves-effect waves-light" 
+                type="submit" name="action" :disabled="!cep || !nome">
+          Consultar
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+    </div>
+    <div class="response" v-if="list">
+       <table>
+        <thead>
+          <tr>
+              <th>CEP</th>
+              <th>Bairro</th>
+              <th>Nome</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, index) in list" :key="index">
+            <td>{{row.cep}}</td>
+            <td>{{row.bairro}}</td>
+            <td>{{row.logradouro}} {{row.complemento}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -17,7 +47,9 @@ export default {
   data() {
     return {
       cep: '', 
-      nome: ''
+      nome: '',
+      resp: '',
+      list: []
     }
   },
   props: {
@@ -25,7 +57,9 @@ export default {
   },
   methods: {
     async enviaCep() {
-      await axios.post('http://localhost:8080/livrese/cep/endereco', {nome:this.nome, cep:this.cep})
+      this.resp = await axios.post('http://localhost:8080/livrese/cep/endereco', {nome:this.nome, cep:this.cep});
+      console.log(this.resp);
+      this.list.push(this.resp.data);
     }
   }
 }
@@ -33,6 +67,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello {
+  padding: 15px;  
+}
 h3 {
   margin: 40px 0 0;
 }
