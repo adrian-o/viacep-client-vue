c86df6673fa7af4fd0007c5cbcec671ec8786faa
Adriano Ohana <adriano.ohana@gmail.com>
2020-07-19 19:55:24 -0300

    Atualizando path da API

--- src/actions/viacep.js
+++ src/actions/viacep.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
     async search (parameters = {}) {
-        const resp = await axios.post('https://viacep-spring-api.herokuapp.com/viacep-cli/cep/endereco', parameters)
+        const resp = await axios.post('https://viacep-spring-api.herokuapp.com/viacep/cep/endereco', parameters)
             .then(response => this.resp = response.data )
             .catch(() => {throw new Error('Falha ao consultar o CEP')})
         return resp                
